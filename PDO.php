b5ef714b45b35d9f3a537e0734e2fd3630278b19
BAUDINO PIERRE <PIE.BAUDINO@pedago.mermoz.edu>
2022-02-02 11:26:41 +0100

    debut query

--- PDO.php
+++ PDO.php
@@ -16,11 +16,16 @@
 //     die();
 //  }
 // 
-$dbhost = 'localhost';
-$dbname = 'hr';
-$dbuser = 'root';
-$dbpass = '';
-$dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass, array(PDO::ATTR_PERSISTENT => true));
+// $dbhost = 'localhost';
+// $dbname = 'hr';
+// $dbuser = 'root';
+// $dbpass = '';
+// $dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass, array(PDO::ATTR_PERSISTENT => true));
 
-
+$sql =  'SELECT name, color, calories FROM fruit ORDER BY name';
+foreach  ($conn->query($sql) as $row) {
+    print $row['name'] . "\t";
+    print  $row['color'] . "\t";
+    print $row['calories'] . "\n";
+}
 ?>
